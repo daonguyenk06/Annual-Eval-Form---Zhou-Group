06d932386acc8561e09a5c99394bdedb79e018d0
Ky Duyen <98281051+daonguyenk06@users.noreply.github.com>
2024-12-19 22:34:17 -0700

    Added DOM ready

--- ResearchPlanResponses/app.js
+++ ResearchPlanResponses/app.js
@@ -1,3 +1,9 @@
 // Firebase imports
 import { database } from "../firebaseConfig.js";
 import { ref, query, orderByChild, equalTo, push, onValue, update, set, remove, child } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-database.js";
+
+//**By wrapping the code inside the DOMContentLoaded event listener, you ensure that the code will only run when the DOM is ready.
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOM fully loaded and parsed.");
+
+});
