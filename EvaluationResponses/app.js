27aa6ae569803b0502e0fc699d26273f53275e65
Ky Duyen <98281051+daonguyenk06@users.noreply.github.com>
2024-12-19 22:25:38 -0700

    Renamed folders and added folders

---
+++ EvaluationResponses/app.js
@@ -0,0 +1,3 @@
+// Firebase imports
+import { database } from "../firebaseConfig.js";
+import { ref, query, orderByChild, equalTo, push, onValue, update, set, remove, child } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-database.js";
